944ef1667bd93e18e8b55ef96ab25c9d42e885c9
Majid Hussain <hussainmajid95@gmail.com>
2017-06-09 09:20:53 +0100

    Add migration to add genres

---
+++ Gig/src/Gig/Migrations/20170609080423_populateGenresTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gig.Migrations
+{
+    public partial class populateGenresTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Jazz')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Blues')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Rock')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Country')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Rap')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Trap')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Hiphop')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Genres Where Name('Jazz',Jazz'Blues','Rock','Country','Rap','Trap','Hiphop')");
+        }                                                              
+    }                                                                  
+}                                                                     
+                                                                     
+                                                                       
+                                                                      
